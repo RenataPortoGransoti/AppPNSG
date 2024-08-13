7b048f25cf4d462e16be6bcba00106a98c07fbdf
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-08-13 08:52:37 -0300

    Página de eventos estilziada e funcionalidade de recarregar página aplicada

--- app_pnsg/lib/Screens/eventos.dart
+++ app_pnsg/lib/Screens/eventos.dart
@@ -1,18 +1,17 @@
 import 'dart:convert';
-import 'package:PNSG/Screens/pastoraisScreen.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import '../config.dart';
-import 'EventoWidget.dart';
 import 'contribua.dart';
 import 'informacoes.dart';
 import 'inicio.dart';
 import 'navigation_bar.dart';
+import 'pastoraisScreen.dart';
 
 class Eventos extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return new EventosState();
+    return EventosState();
   }
 }
 
@@ -26,6 +25,7 @@ class EventosState extends State<Eventos> {
     super.initState();
     fetchEventos();
   }
+
   Future<void> fetchEventos() async {
     try {
       final response = await http.get(Uri.parse('${Config.baseUrl}eventosapi'));
@@ -43,6 +43,25 @@ class EventosState extends State<Eventos> {
       });
       print(e);
     }
+  }
+
+  Future<void> _handleRefresh() async {
+    await fetchEventos();
+  }
+
+  String formatDateTime(String dateTime) {
+    final date = DateTime.parse(dateTime);
+    return '${date.day}/${date.month}/${date.year} ${date.hour}:${date.minute}';
+  }
+
+  String formatDate(String dateTime) {
+    final date = DateTime.parse(dateTime);
+    return '${date.day.toString().padLeft(2, '0')}/${date.month.toString().padLeft(2, '0')}/${date.year}';
+  }
+
+  String formatTime(String dateTime) {
+    final date = DateTime.parse(dateTime);
+    return '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
   }
 
   @override
@@ -74,8 +93,8 @@ class EventosState extends State<Eventos> {
               );
             } else if (currentPageIndex == 4) {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Informacoes())
+                context,
+                MaterialPageRoute(builder: (context) => Informacoes()),
               );
             }
           });
@@ -83,63 +102,104 @@ class EventosState extends State<Eventos> {
         selectedIndex: currentPageIndex,
         backgroundColor: Colors.lightBlue,
       ),
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-        child: Column(
+      body: RefreshIndicator(
+        onRefresh: _handleRefresh,
+        color: Colors.blue[200], // Cor sólida para o indicador de refresh
+        child: ListView(
+          padding: EdgeInsets.all(10),
           children: [
-        Container(
-        margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
-        padding: const EdgeInsets.all(18),
-        width: MediaQuery.of(context).size.width - 40,
-        decoration: BoxDecoration(
-          color: Color(0xFF036896),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          children: [
-            Text(
-              "EVENTOS PAROQUIAIS",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+            Container(
+              padding: const EdgeInsets.all(18),
+              margin: EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 10),
+              decoration: BoxDecoration(
+                color: Color(0xFF036896),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Center(
+                child: Text(
+                  "EVENTOS PAROQUIAIS",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
               ),
             ),
-        ],
-        ),
-    ),
             SizedBox(height: 10.0),
-            // ElevatedButton(
-            //   onPressed: () {
-            //     // Ação ao clicar em "Ver eventos salvos"
-            //   },
-            //   child: Text('Ver eventos salvos', style: TextStyle(
-            //     fontSize: 18,
-            //     color: Colors.white,
-            //     fontWeight: FontWeight.bold,
-            //   ),),
-            //   style: ElevatedButton.styleFrom(
-            //     backgroundColor: Colors.lightBlue,
-            //   ),
-            // ),
-            ListView.separated(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: eventos.length,
-              padding: EdgeInsets.all(5),
-              separatorBuilder: (context, index) => SizedBox(height: 15.0),
-              itemBuilder: (context, index) {
-                final evento = eventos[index];
-                return EventoWidget(
-                  nome_evento: evento['nome_evento'],
-                  data_inicio: evento['data_inicio'],
-                  data_fim: evento['data_fim'],
-                  local: evento['local'],
-                  descricao: evento['descricao'],
+            isLoading
+                ? Center(child: CircularProgressIndicator())
+                : Column(
+              children: eventos.map<Widget>((evento) {
+                return SizedBox(
+                  width: 350, // Define a largura fixa para o card
+                  child: Card(
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        gradient: LinearGradient(
+                          colors: [Colors.blue[100]!, Colors.white],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            evento['nome_evento'],
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Início: ${formatDate(evento['data_inicio'])} ${formatTime(evento['data_inicio'])}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                          Text(
+                            'Fim: ${formatDate(evento['data_fim'])} ${formatTime(evento['data_fim'])}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[700],
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Local: ${evento['local']}',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xFF036896),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            evento['descricao'],
+                            maxLines: 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 );
-              },
+              }).toList(),
             ),
           ],
         ),
@@ -147,3 +207,20 @@ class EventosState extends State<Eventos> {
     );
   }
 }
+
+
+
+
+// ElevatedButton(
+//   onPressed: () {
+//     // Ação ao clicar em "Ver eventos salvos"
+//   },
+//   child: Text('Ver eventos salvos', style: TextStyle(
+//     fontSize: 18,
+//     color: Colors.white,
+//     fontWeight: FontWeight.bold,
+//   ),),
+//   style: ElevatedButton.styleFrom(
+//     backgroundColor: Colors.lightBlue,
+//   ),
+// ),
