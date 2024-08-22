6c70e97d79c91de6093ec6bf88cceb24c07503f1
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-08-22 14:20:09 -0300

    Retirada de um css não utilizado

--- app_pnsg_laravel/resources/views/layouts/app.blade.php
+++ app_pnsg_laravel/resources/views/layouts/app.blade.php
@@ -7,8 +7,6 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css"
         integrity="sha256-Qsx5lrStHZyR9REqhUF8iQt73X06c8LGIUPzpOhwRrI=" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../dist/css/adminlte.css">
-
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
