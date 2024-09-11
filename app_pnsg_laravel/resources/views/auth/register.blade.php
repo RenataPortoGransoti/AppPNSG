bd2f8cc3f18da05166916f8f492d176015b8b09f
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-09-11 20:42:04 -0300

    Tela de registro ajustada com novos campos

--- app_pnsg_laravel/resources/views/auth/register.blade.php
+++ app_pnsg_laravel/resources/views/auth/register.blade.php
@@ -2,25 +2,43 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="name" :value="__('Nome')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required
                 autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="cpf" :value="__('CPF')" />
+            <x-text-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')"
+                required autofocus autocomplete="cpf" />
+            <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="celular" :value="__('Celular')" />
+            <x-text-input id="celular" class="block mt-1 w-full" type="text" name="celular" :value="old('celular')"
+                required autofocus autocomplete="celular" />
+            <x-input-error :messages="$errors->get('celular')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('E-mail')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
-                required autocomplete="username" />
+                required autocomplete="email" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="dataNascimento" :value="__('Data de Nascimento')" />
+            <x-text-input id="dataNascimento" class="block mt-1 w-full" type="dataNascimento" name="dataNascimento"
+                :value="old('dataNascimento')" required autocomplete="dataNascimento" />
+            <x-input-error :messages="$errors->get('dataNascimento')" class="mt-2" />
+        </div>
+
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Senha')" />
 
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                 autocomplete="new-password" />
@@ -28,9 +46,8 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmar Senha')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
                 name="password_confirmation" required autocomplete="new-password" />
@@ -38,12 +55,7 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
+        <div class="flex items-center justify-center mt-8">
             <x-primary-button class="ms-4">
                 {{ __('Registrar') }}
             </x-primary-button>
--- app_pnsg_laravel/resources/views/components/application-logo.blade.php
+++ app_pnsg_laravel/resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('build/assets/brasao_pnsg.png') }}" class="rounded-full w-5/6">
+<img src="{{ asset('build/assets/brasao_pnsg.png') }}" class="rounded-full w-4/6">
