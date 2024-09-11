a266a762787f01664762140681847e63f6e77457
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-09-11 19:48:03 -0300

    Botão para redirecionar para tela de registro

--- app_pnsg_laravel/resources/views/dashboard.blade.php
+++ app_pnsg_laravel/resources/views/dashboard.blade.php
@@ -1,4 +1,14 @@
-
 @extends('layouts.app')
 
-    <title>Dashboard | Painel Admin</title>
+<title>Dashboard | Painel Admin</title>
+@section('content')
+
+    <body>
+        <div class="flex justify-center mt-6">
+            <a href="{{ route('register') }}"
+                class="rounded-md px-6 py-4 bg-[#036896] text-white font-semibold hover:bg-[#9DDEFB] hover:text-black transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#036896] focus:ring-offset-2">
+                Registrar novo(a) secretário(a)
+            </a>
+        </div>
+    </body>
+@endsection
