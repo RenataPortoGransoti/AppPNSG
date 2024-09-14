f4b2a754ed601e3c769917f7d9984a9a9a766ff6
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-09-13 21:03:11 -0300

    Tabela de usuários atualizados com dados para secretário

--- app_pnsg_laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ app_pnsg_laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
+            $table->string('cpf')->unique();
+            $table->string('celular');
             $table->string('email')->unique();
+            $table->date('dataNascimento');
+            $table->string('tipo')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
