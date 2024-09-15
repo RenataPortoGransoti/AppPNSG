c8a8cc495e9fd8defd7911d9e1e21adac76d74b6
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-09-14 22:32:44 -0300

    placeholder

--- app_pnsg_laravel/resources/views/auth/register.blade.php
+++ app_pnsg_laravel/resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
         @csrf
 
         <div>
-            <x-input-label for="nome" :value="__('Nome*')" />
+            <x-input-label for="nome" :value="__('Nome Completo*')" />
             <x-text-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required
                 autofocus autocomplete="nome" />
             <x-input-error :messages="$errors->get('nome')" class="mt-2" />
@@ -12,14 +12,14 @@
         <div class="mt-4">
             <x-input-label for="cpf" :value="__('CPF*')" />
             <x-text-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')"
-                required />
+                placeholder="000.000.000-00" required />
             <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
         </div>
 
         <div class="mt-4">
             <x-input-label for="celular" :value="__('Celular*')" />
             <x-text-input id="celular" class="block mt-1 w-full" type="text" name="celular" :value="old('celular')"
-                required />
+                placeholder="(00) 00000-0000" required />
             <x-input-error :messages="$errors->get('celular')" class="mt-2" />
         </div>
 
@@ -33,7 +33,7 @@
         <div class="mt-4">
             <x-input-label for="dataNascimento" :value="__('Data de Nascimento*')" />
             <x-text-input id="dataNascimento" class="block mt-1 w-full" type="text" name="dataNascimento"
-                :value="old('dataNascimento')" required />
+                placeholder="dd/mm/aaaa" :value="old('dataNascimento')" required />
             <x-input-error :messages="$errors->get('dataNascimento')" class="mt-2" />
         </div>
 
