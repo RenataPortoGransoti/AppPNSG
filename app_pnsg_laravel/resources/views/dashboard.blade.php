b86c1fd40838d30073a600f1b1e0f31506e5982c
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-09-14 21:40:54 -0300

    nome do botão

--- app_pnsg_laravel/resources/views/dashboard.blade.php
+++ app_pnsg_laravel/resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="flex justify-between items-center mb-4">
             <a href="{{ route('register') }}"
                 class="rounded-md px-6 py-4 bg-[#036896] text-white font-semibold hover:bg-[#9DDEFB] hover:text-black transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#036896] focus:ring-offset-2">
-                Registrar novo(a) usuário(a)
+                Registrar novo(a) secretário(a)
             </a>
 
             <form method="GET" action="{{ route('dashboard') }}" class="flex items-center">
