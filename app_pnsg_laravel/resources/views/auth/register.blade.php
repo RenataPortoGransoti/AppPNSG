fcfb1f9e86b027fd16361faef18811c2c08862c4
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-09-14 22:19:40 -0300

    Asteriscos em campos obrigatórios para registro

--- app_pnsg_laravel/resources/views/auth/register.blade.php
+++ app_pnsg_laravel/resources/views/auth/register.blade.php
@@ -3,42 +3,42 @@
         @csrf
 
         <div>
-            <x-input-label for="nome" :value="__('Nome')" />
+            <x-input-label for="nome" :value="__('Nome*')" />
             <x-text-input id="nome" class="block mt-1 w-full" type="text" name="nome" :value="old('nome')" required
                 autofocus autocomplete="nome" />
             <x-input-error :messages="$errors->get('nome')" class="mt-2" />
         </div>
 
         <div class="mt-4">
-            <x-input-label for="cpf" :value="__('CPF')" />
+            <x-input-label for="cpf" :value="__('CPF*')" />
             <x-text-input id="cpf" class="block mt-1 w-full" type="text" name="cpf" :value="old('cpf')"
                 required />
             <x-input-error :messages="$errors->get('cpf')" class="mt-2" />
         </div>
 
         <div class="mt-4">
-            <x-input-label for="celular" :value="__('Celular')" />
+            <x-input-label for="celular" :value="__('Celular*')" />
             <x-text-input id="celular" class="block mt-1 w-full" type="text" name="celular" :value="old('celular')"
                 required />
             <x-input-error :messages="$errors->get('celular')" class="mt-2" />
         </div>
 
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('E-mail*')" />
             <x-text-input id="email" class="block mt-1 w-full" type="text" name="email" :value="old('email')"
                 required />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="mt-4">
-            <x-input-label for="dataNascimento" :value="__('Data de Nascimento')" />
+            <x-input-label for="dataNascimento" :value="__('Data de Nascimento*')" />
             <x-text-input id="dataNascimento" class="block mt-1 w-full" type="text" name="dataNascimento"
                 :value="old('dataNascimento')" required />
             <x-input-error :messages="$errors->get('dataNascimento')" class="mt-2" />
         </div>
 
         <div class="mt-4 relative">
-            <x-input-label for="password" :value="__('Senha')" />
+            <x-input-label for="password" :value="__('Senha*')" />
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
             <i class="bx bx-lock-alt absolute right-3 top-10 cursor-pointer" id="togglePasswordIcon"
                 onclick="togglePasswordVisibility('password', 'togglePasswordIcon')"></i>
@@ -46,7 +46,7 @@
         </div>
 
         <div class="mt-4 relative">
-            <x-input-label for="password_confirmation" :value="__('Confirmar Senha')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmar Senha*')" />
             <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
                 name="password_confirmation" required />
             <i class="bx bx-lock-alt absolute right-3 top-10 cursor-pointer" id="togglePasswordConfirmIcon"
