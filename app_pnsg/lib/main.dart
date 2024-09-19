082e5c92d391d526ade7b6df97181f96a7c2be21
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-09-19 08:53:54 -0300

    Refatoração main

--- app_pnsg/lib/main.dart
+++ app_pnsg/lib/main.dart
@@ -1,22 +1,41 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:PNSG/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'Screens/pastoraisScreen.dart';
+import 'Screens/PastoralEspecifica.dart';
 import 'Screens/inicio.dart';
+import 'Screens/eventos.dart';
+import 'Screens/contribua.dart';
+import 'Screens/informacoes.dart';
+import 'config.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(AppPnsg());
 }
 
-class MyApp extends StatelessWidget {
+class AppPnsg extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meu App',
+      title: 'App PNSG',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Inicio(),
+      home: Splash(), // Tela inicial de splash
+      routes: {
+        '/Inicio': (context) => Inicio(),
+        '/PastoraisScreen': (context) => PastoraisScreen(),
+        '/Eventos': (context) => Eventos(),
+        '/Contribua': (context) => Contribua(),
+        '/Informacoes': (context) => Informacoes(),
+      },
     );
   }
 }
+
+
