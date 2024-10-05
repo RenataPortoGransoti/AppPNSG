e3345a4042805eacf6cf857e9b89542b19eb2429
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-10-04 21:44:35 -0300

    Estilização botão registrar

--- app_pnsg_laravel/resources/views/dashboard.blade.php
+++ app_pnsg_laravel/resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="container mx-auto p-4">
         <div class="flex justify-between items-center mb-4">
             <a href="{{ route('register') }}"
-                class="rounded-md px-6 py-4 bg-[#036896] text-white font-semibold hover:bg-[#9DDEFB] hover:text-black transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#036896] focus:ring-offset-2">
+                class="rounded-md px-6 py-4 bg-[#9DDEFB] text-black font-semibold hover:bg-[#036896] hover:text-white transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#036896] focus:ring-offset-2">
                 Registrar novo(a) secretário(a)
             </a>
 
