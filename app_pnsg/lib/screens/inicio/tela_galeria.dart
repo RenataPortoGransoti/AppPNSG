9cda35e36f1d0884491130a4eb0ed33d5459e6fc
Renata Porto Gransoti <renata.gransoti@edu.unifil.br>
2024-10-14 09:45:16 -0300

    Tela galeria mostrando drive de fotos

--- app_pnsg/lib/screens/inicio/tela_galeria.dart
+++ app_pnsg/lib/screens/inicio/tela_galeria.dart
@@ -1,106 +1,57 @@
 import 'package:flutter/material.dart';
-import '../tela_contribua.dart';
-import '../tela_eventos.dart';
-import '../tela_informacoes.dart';
-import '../tela_inicio.dart';
-import '../navigation_bar.dart';
-import '../tela_pastorais.dart';
+import 'package:webview_flutter/webview_flutter.dart'; // Certifique-se de importar o pacote correto
 
 class Galeria extends StatefulWidget {
   const Galeria({super.key});
 
   @override
-  GaleriaState createState() => GaleriaState();
+  _GaleriaState createState() => _GaleriaState();
 }
 
-class GaleriaState extends State<Galeria> {
-  int currentPageIndex = 0;
+class _GaleriaState extends State<Galeria> {
+  late final WebViewController _controller;
+
+  final String googleDriveFolderUrl =
+      'https://drive.google.com/drive/folders/1bczfTHgOD7mwodZ89e4SM7qvlamzQeM5?usp=sharing';
 
   @override
   void initState() {
     super.initState();
-
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(googleDriveFolderUrl));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: CustomBottomNavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-            if (currentPageIndex == 0) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Inicio()),
-              );
-            } else if (currentPageIndex == 1) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const PastoraisScreen()),
-              );
-            } else if (currentPageIndex == 2) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Eventos()),
-              );
-            } else if (currentPageIndex == 3) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Contribua()),
-              );
-            } else if (currentPageIndex == 4) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const Informacoes()),
-              );
-            }
-          });
-        },
-        selectedIndex: currentPageIndex,
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: const Column(
+      backgroundColor: Colors.white,
+      body: Column(
         children: [
-          Expanded(child: GaleriaPagina()), // Ocupa o espaço restante
+          Container(
+            width: double.infinity,
+            margin: const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 20),
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: const Color(0xFF036896).withOpacity(0.8),
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: const Text(
+              'Galeria',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ),
+          Expanded(
+            child: WebViewWidget(controller: _controller),
+          ),
         ],
       ),
     );
-  }
-}
-
-const String nome = 'Galeria';
-
-class GaleriaPagina extends StatelessWidget {
-  final String googleDriveFolderUrl =
-      'https://drive.google.com/drive/folders/YOUR_FOLDER_ID?usp=sharing';
-
-  const GaleriaPagina({super.key}); // Substitua pelo link da sua pasta
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: double.infinity,
-          margin: const EdgeInsets.only(left: 20, right: 20, top: 50, bottom: 20),
-          padding: const EdgeInsets.all(10),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: const Color(0xFF036896).withOpacity(0.8),
-            borderRadius: BorderRadius.circular(50),
-          ),
-          child: const Text(
-            nome,
-            style: TextStyle(
-              fontSize: 24,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-]
-        );
-
   }
 }
